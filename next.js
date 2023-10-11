2d83277e125ec5be8cdd3b373d043d35d8844c64
Snarloff <snarloff@gmail.com>
2023-10-11 18:57:28 -0300

    test: :test_tube: testando se com o arquivo .prettierrc ele fixa sem problemas

--- next.js
+++ next.js
@@ -1,99 +1,51 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
-    jest: true,
+    node: true,
   },
-  extends: [
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["jsx-a11y", "@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        trailingComma: "none",
-        tabWidth: 2,
-        singleQuote: true,
-        charset: "utf-8",
-        jsxSingleQuote: true,
-        jsxBracketSameLine: true,
-        semi: false,
-        bracketSameLine: true,
-        bracketSpacing: true,
-        arrowParens: "always",
-        printWidth: 150,
-        useTabs: false,
-        endOfLine: "auto",
-      },
-    ],
-    "import/order": [
+    'prettier/prettier': ['error', './prettierrc'],
+    'import/order': [
       1,
       {
-        groups: [
-          "external",
-          "builtin",
-          "internal",
-          "sibling",
-          "parent",
-          "index",
-        ],
+        groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'],
         pathGroups: [
           {
-            pattern: "components",
-            group: "internal",
+            pattern: 'components',
+            group: 'internal',
           },
           {
-            pattern: "common",
-            group: "internal",
+            pattern: 'common',
+            group: 'internal',
           },
           {
-            pattern: "routes/**",
-            group: "internal",
+            pattern: 'routes/**',
+            group: 'internal',
           },
           {
-            pattern: "assets/**",
-            group: "internal",
-            position: "after",
+            pattern: 'assets/**',
+            group: 'internal',
+            position: 'after',
           },
         ],
-        pathGroupsExcludedImportTypes: ["internal"],
+        pathGroupsExcludedImportTypes: ['internal'],
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
       },
     ],
-    "jsx-a11y/alt-text": [
-      "warn",
-      {
-        elements: ["img"],
-        img: ["Image"],
-      },
-    ],
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-    "react/no-unknown-property": "error",
   },
   settings: {
-    react: {
-      version: "detect",
-    },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
   },
 };
